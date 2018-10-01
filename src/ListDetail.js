e550c77f47b58a9d97406381abb6ea1884f388a6
umut <umut@canbolat>
2018-10-01 03:57:33 +0300

    Adding dependencies funcitonality has been added. Show dependencies button is added. To-do items which have dependency can not be completed if dependent to-do item is not completed.

--- src/ListDetail.js
+++ src/ListDetail.js
@@ -25,7 +25,7 @@ export class ListDetail extends Component{
             this.setState({
               data: responseJson
             });
-          });
+        });
     }
 
   deleteList(e){
@@ -72,7 +72,7 @@ export class ListDetail extends Component{
             </tbody>
           </table>
 
-        <AddItemForm listId={listId} changeData={this.updateListData} />
+        <AddItemForm listId={listId} changeData={this.updateListData} items={this.state.data}/>
 
       </div>
 
@@ -94,39 +94,73 @@ class AddItemForm extends Component{
 
     this.addItem = this.addItem.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleDepsChange = this.handleDepsChange.bind(this);
+    this.addDependency = this.addDependency.bind(this);
   }
 
   addItem(e){
     const apiUrl = "http://localhost:8080/addItem";
     e.preventDefault();
 
-
-
     var params = this.state;
-
     var formData = new FormData();
-
     for (var k in params) {
         formData.append(k, params[k]);
     }
-
     var request = {
         method: 'POST',
         body: formData
     };
+console.log("in other: " + this.state.dependencies);
     fetch(apiUrl, request).then(response => {
       if (response.ok) {
-        this.setState({
-          itemName: "",
-          list: this.props.listId,
-          itemDesc: "",
-          deadline: "",
-          dependencies: []
+        response.json().then(responseJson => {
+          console.log(responseJson.itemId);
+          this.addDependency(responseJson.itemId);
         });
-        this.props.changeData(this.state.list);
-      }
 
+      }
     });
+  }
+
+  addDependency(itemId){
+    console.log("in adddependency: " + this.state.dependencies);
+    const apiUrl = "http://localhost:8080/addDependency";
+    var deps=this.state.dependencies;
+    console.log("DEP SIZE1: " + deps.length);
+    if(deps.length < 1){
+      this.setState({
+        itemName: "",
+        list: this.props.listId,
+        itemDesc: "",
+        deadline: "",
+        dependencies: []
+      });
+      this.props.changeData(this.state.list);
+    }else{
+      for(var dep in deps){
+        console.log("DEP:"+deps[dep]);
+        var formData = new FormData();
+          formData.append("todoItem", itemId);
+          formData.append("dependentTo", deps[dep]);
+          var request = {
+            method: 'POST',
+            body: formData
+          };
+        fetch(apiUrl, request).then(response =>{
+            if(dep == deps.length-1){
+              this.setState({
+                itemName: "",
+                list: this.props.listId,
+                itemDesc: "",
+                deadline: "",
+                dependencies: []
+              });
+              this.props.changeData(this.state.list);
+            }
+        });
+      }
+    }
   }
 
   handleInputChange(e){
@@ -134,34 +168,73 @@ class AddItemForm extends Component{
       this.setState({
         [target.name]: target.value
       });
+  }
 
+  handleDepsChange(e){
+    const target = e.target;
+
+    var dependencies = [];
+    for (var i = 0; i < target.options.length; i++) {
+        if (target.options[i].selected) dependencies.push(target.options[i].value);
     }
 
+    this.setState({
+      dependencies: dependencies
+    });
+  }
+
   render(){
+
+    const depItems = this.props.items.map(item => {
+      return <option value={item.itemId}>{item.itemName}</option>;
+    });
+
     return(
       <form onSubmit={this.addItem}>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Deadline</th>
-            <th scope="col">Dependencies</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr>
-
-          <td><input type="text" className="form-control form-control-sm" name="itemName" placeholder="name" onChange={this.handleInputChange} value={this.state.itemName} /></td>
-          <td><input type="text" className="form-control form-control-sm" name="itemDesc" placeholder="description" onChange={this.handleInputChange} value={this.state.itemDesc} /></td>
-          <td><input type="text" className="form-control form-control-sm" name="deadline" placeholder="23-01-2019" onChange={this.handleInputChange} value={this.state.deadline} /></td>
-          <td> add dependency</td>
-          <td><button type="submit" className="btn btn-primary btn-sm">Add</button>  </td>
-
-        </tr>
-        </tbody>
-      </table>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Deadline</th>
+              <th scope="col">Dependencies</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="text" className="form-control form-control-sm" name="itemName" placeholder="name" onChange={this.handleInputChange} value={this.state.itemName} /></td>
+              <td><input type="text" className="form-control form-control-sm" name="itemDesc" placeholder="description" onChange={this.handleInputChange} value={this.state.itemDesc} /></td>
+              <td><input type="text" className="form-control form-control-sm" name="deadline" placeholder="23-01-2019" onChange={this.handleInputChange} value={this.state.deadline} /></td>
+              <td>
+                <button type="button" className="btn btn-outline-primary  btn-sm" data-toggle="modal" data-target={"#addDependencyModal" + this.state.list}>
+                  Add dependency
+                </button>
+                <div className="modal fade" id={"addDependencyModal" + this.state.list} tabindex="-1" role="dialog" aria-labelledby="Add Dependency" aria-hidden="true">
+                  <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="addDependencyModalLongTitle">Add Dependency</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <select multiple value={this.state.dependencies} className="form-control" id="sel" onChange={this.handleDepsChange}>
+                          {depItems}
+                        </select>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-primary btn-sm" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td><button type="submit" className="btn btn-primary btn-sm">Create item</button>  </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
     );
   }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -29,18 +29,79 @@ export class TodoItem extends Component{
       });
   }
 
-
   render(){
-    const icon_times = <i className="fa fa-times"></i>
+    const icon_times = <i className="fa fa-times"></i>;
+
+    const dependentItems = this.props.item.dependencies.map(dep => {
+      return <DependentItem itemId={dep.dependentTo} />
+
+    });
+
+    var show_deps_button = (
+      <div>
+        <button type="button" className="btn btn-outline-primary  btn-sm" data-toggle="modal" data-target={"#showDependencyModal" + this.props.item.itemId}>
+          Show dependencies
+        </button>
+        <div className="modal fade" id={"showDependencyModal"  + this.props.item.itemId} tabindex="-1" role="dialog" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="showDependencyModalLongTitle">Dependencies</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <ul>{dependentItems}</ul>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary btn-sm" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 
     return (
-      <tr onClick={this.changeItemStatus} className={this.props.item.status ? "disabled" : "umut"}>
-        <td>{this.props.item.itemName}</td>
-        <td>{this.props.item.itemDesc}</td>
-        <td>{this.props.item.deadline}</td>
-        <td>{this.props.item.dependencies.length > 0 ? "show deps" : "-"}</td>
+      <tr className={this.props.item.status ? "disabled" : "umut"}>
+        <td onClick={this.changeItemStatus}>{this.props.item.itemName}</td>
+        <td onClick={this.changeItemStatus}>{this.props.item.itemDesc}</td>
+        <td onClick={this.changeItemStatus}>{this.props.item.deadline}</td>
+        <td>{this.props.item.dependencies.length > 0 ? show_deps_button : "-"}</td>
         <td><button onClick={this.deleteItem} className="btn btn-outline-danger btn-sm">{icon_times}</button></td>
       </tr>
+    );
+  }
+}
+
+class DependentItem extends Component{
+  constructor(props){
+    super(props);
+    this.getDependentDetails = this.getDependentDetails.bind(this);
+    this.state = {
+      depDetail: ""
+    };
+  }
+
+  componentDidMount(){
+    this.getDependentDetails();
+  }
+
+  getDependentDetails(){
+    const apiUrl = 'http://localhost:8080/getItemById?itemId=' + this.props.itemId;
+    fetch(apiUrl)
+      .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            depDetail: responseJson.itemName
+          });
+        });
+  }
+
+  render(){
+    return(
+      <li>{this.state.depDetail}</li>
     );
   }
 }
